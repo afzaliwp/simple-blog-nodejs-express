77ce1c89db9c460b0fd5df5a8b6365cae67afa34
Mohammad Afzali <info@afzaliwp.com>
2021-12-03 22:56:57 +0330

    [done] HandleBars Template engine configurations

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+require('./bootstrap')(app);
+
+app.get('/', (req, res) => {
+    res.render('index', { layout: false, userName: 'MA90' });
+});
+
 module.exports = () => {
     const port = process.env.APP_PORT;
     app.listen(port, () => {
