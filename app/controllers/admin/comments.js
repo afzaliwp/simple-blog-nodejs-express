ff9586582b24396d5e8a26f834c7b15a88d0bb92
MA90 <rahgozar1210@gmail.com>
2021-12-17 10:20:19 +0330

    Show current comment status with helpers

--- app/controllers/admin/comments.js
+++ app/controllers/admin/comments.js
@@ -1,5 +1,21 @@
 const commentsModel = require('@models/comments');
 exports.index = async(req, res) => {
     const allComments = await commentsModel.getAllComments();
-    res.render('admin/comments/index', { layout: 'admin', allComments });
+    res.render('admin/comments/index', {
+        layout: 'admin',
+        allComments,
+        helpers: {
+            checkStatus: function(status, options) {
+                if (status == 0) {
+                    return options.fn({ class: 'warning', icon: 'zmdi zmdi-eye', text: 'در انتظار بازبینی' });
+                }
+                if (status == 1) {
+                    return options.fn({ class: 'success', icon: 'zmdi zmdi-check', text: 'تایید' });
+                }
+                if (status == 2) {
+                    return options.fn({ class: 'danger', icon: 'zmdi zmdi-close', text: 'رد شده' });
+                }
+            }
+        }
+    });
 }
