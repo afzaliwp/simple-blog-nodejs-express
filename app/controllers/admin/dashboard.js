744f5062d986c87ebd65a62a1e1f68c791bdddf0
MA90 <rahgozar1210@gmail.com>
2022-01-06 10:17:19 +0330

    Create draft post from

--- app/controllers/admin/dashboard.js
+++ app/controllers/admin/dashboard.js
@@ -1,6 +1,8 @@
 const statistics = require('@models/statistics');
 const dateService = require('@services/dateService');
 const commentModel = require('@models/comments');
+const postsModel = require('@models/posts');
+
 exports.index = async(req, res) => {
     const latestComments = await commentModel.getLatestComments();
     const presentedComments = latestComments.map((comment) => {
@@ -15,4 +17,32 @@ exports.index = async(req, res) => {
         latestComments: presentedComments
     }
     res.adminRender('admin/dashboard/index', {...data });
+}
+
+exports.createDraftPost = async(req, res) => {
+    const postData = {
+        title: req.body.title,
+        slug: 'change the slug',
+        content: req.body.content,
+        author_id: req.session.user.ID,
+        status: 2,
+    }
+    const errors = [];
+    if (postData.title === '') {
+        errors.push('عنوان نمیتواند خالی باشد.');
+    }
+    if (postData.content === '') {
+        errors.push('محتوا نمیتواند خالی باشد.');
+    }
+
+    if (errors.length > 0) {
+        req.flash('errors', errors);
+        return res.redirect('/admin/dashboard#draftForm');
+    }
+
+    const newPostID = await postsModel.create(postData);
+    if (newPostID > 0) {
+        req.flash('success', ['پیش نویس با موفقیت ایجاد شد. آن را از طریق ویرایش مطلب ویرایش کنید یا انتشار دهید.']);
+        return res.redirect('/admin/dashboard#draftForm');
+    }
 }
--- app/routes/admin/dashboard.js
+++ app/routes/admin/dashboard.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('@controllers/admin/dashboard');
 router.get('/', dashboardController.index);
-
+router.post('/create-draft-post', dashboardController.createDraftPost);
 module.exports = router;
