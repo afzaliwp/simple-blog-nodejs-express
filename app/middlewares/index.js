f80cd8cb88735ebb55003f5579050f6a3fc5decf
MA90 <rahgozar1210@gmail.com>
2022-01-09 16:20:35 +0330

    Initialize Front and homepage

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -3,6 +3,12 @@ module.exports = app => {
     app.use((req, res, next) => {
         const errors = req.flash('errors');
         const success = req.flash('success');
+
+        res.frontRender = (template, options) => {
+            options = {...options, layout: 'front' };
+            res.render(template, options);
+        }
+
         res.adminRender = (template, options) => {
             const currentUser = req.session.user;
             if (currentUser) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@ const authRouter = require('./auth');
 const authMiddleware = require('@middlewares/auth');
 const guestMiddleware = require('@middlewares/guest');
 const logoutController = require('@controllers/auth/logout');
+const homeController = require('@controllers/front/home');
 module.exports = app => {
+    app.get('/', homeController.index);
     app.use('/admin', [authMiddleware], adminRouter);
     app.use('/auth', [guestMiddleware], authRouter);
     app.get('/logout', logoutController.logout);
